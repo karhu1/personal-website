35291a28c60f92ed92627bc7ab287d9cefec2898
Rasmus <rasmus.makiniemi@gmail.com>
2023-12-10 15:42:22 -0800

    progress on animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import Header from "./components/Header";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 
 function App() {
+  let options = {
+    // root: document.querySelector("#scrollArea"),
+    rootMargin: "300px 0px -75px 0px",
+    threshold: 0.4,
+  };
+
+  useEffect(() => {
+    let observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        console.log('entry: ')
+        console.log(entry)
+        entry.target.classList.toggle('show', entry.isIntersecting)
+      })
+    }, options);
+
+    let elements = document.querySelectorAll(".hidden");
+    elements.forEach((el) => observer.observe(el))
+  }, [])
+
+  AOS.init();
+
   return (
     <>
       <Header />
@@ -9,12 +33,13 @@ function App() {
         <h1><span id="first">RASMUS</span><br/>MAKINIEMI</h1>
       </div>
       <div className="info">
-        <p className="bio">
+        <p className="bio hidden">
           I am a Computer Science teaching assistant and student at the <a target="_blank" href="https://www.cs.washington.edu/">
           University of Washignton's Paul G. Allen School</a>. Currently pursuing
           my passion for web development, teaching, and environmental sustainability.
         </p>
       </div>
+      <div className="hidden"></div>
     </>
   );
 }
