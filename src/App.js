8c0534a613d1b45b2b638f3cd55925278529e34f
rasmus1 <rasmus1@uw.edu>
2023-12-10 13:51:30 -0800

    starting work on description

--- src/App.js
+++ src/App.js
@@ -7,9 +7,14 @@ function App() {
       <Header />
       <div className="info">
         <h1><span id="first">RASMUS</span><br/>MAKINIEMI</h1>
-        <p>Testing</p>
       </div>
-      <div className="info"></div>
+      <div className="info">
+        <p className="bio">
+          I am a Computer Science teaching assistant and student at the <a target="_blank" href="https://www.cs.washington.edu/">
+          University of Washignton's Paul G. Allen School</a>. Currently pursuing
+          my passion for web development, teaching, and environmental sustainability.
+        </p>
+      </div>
     </>
   );
 }
