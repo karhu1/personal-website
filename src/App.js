2e0bdadcd86f5423ed3ee65c5eb8d06473c62801
Rasmus <rasmus.makiniemi@gmail.com>
2023-12-10 20:24:58 -0800

    animations progress

--- src/App.js
+++ src/App.js
@@ -11,17 +11,30 @@ function App() {
     threshold: 0.4,
   };
 
+  let options2 = {
+    // root: document.querySelector("#scrollArea"),
+    rootMargin: "2000px 0px 400px 0px",
+    threshold: 0.5,
+  };
+
   useEffect(() => {
     let observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        console.log('entry: ')
-        console.log(entry)
-        entry.target.classList.toggle('show', entry.isIntersecting)
+        entry.target.classList.toggle('fadeInAnim', entry.isIntersecting)
       })
     }, options);
 
+    let observer2 = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle('move-up', entry.isIntersecting)
+        if (entry.isIntersecting) console.log('meow')
+      })
+    }, options2);
+
     let elements = document.querySelectorAll(".hidden");
+    let elements2 = document.querySelectorAll(".project-container");
     elements.forEach((el) => observer.observe(el))
+    elements2.forEach((el) => observer2.observe(el))
   }, [])
 
   AOS.init();
@@ -29,17 +42,22 @@ function App() {
   return (
     <>
       <Header />
-      <div className="info">
+      <div className="info-container">
         <h1><span id="first">RASMUS</span><br/>MAKINIEMI</h1>
       </div>
-      <div className="info">
+      <div className="bio-container">
         <p className="bio hidden">
           I am a Computer Science teaching assistant and student at the <a target="_blank" href="https://www.cs.washington.edu/">
-          University of Washignton's Paul G. Allen School</a>. Currently pursuing
+          University of Washington's Paul G. Allen School</a>. Currently pursuing
           my passion for web development, teaching, and environmental sustainability.
         </p>
       </div>
-      <div className="hidden"></div>
+      {/* <div className="project-container">
+        
+      </div> */}
+      <div className="project-container">
+        <h2>PROJECTS</h2>
+      </div>
     </>
   );
 }
