88006d0431599d55a0b4c67102472dadf5988443
rasmus1 <rasmus1@uw.edu>
2024-03-25 09:14:12 -0700

    worked with media queries a little bit more

--- src/components/Testing.js
+++ src/components/Testing.js
@@ -23,22 +23,30 @@ const Testing = () => {
   return (
     <div>
       <Parallax pages={4}>
-        <ParallaxLayer offset={0} speed={0.2} className="info-container">
-          <h1><span id="first">RASMUS</span><br/>MAKINIEMI</h1>
+        <ParallaxLayer offset={0} speed={0.1} className="info-container">
+          <h1>
+            <span id="first">RASMUS</span>
+            <br />
+            MAKINIEMI
+          </h1>
           <p className="bio">
-            I am a Computer Science teaching assistant and student at the <a target="_blank" href="https://www.cs.washington.edu/">
-            University of Washington's Paul G. Allen School</a>. Currently pursuing
-            my passion for web development, teaching, and environmental sustainability.
+            I am a Computer Science teaching assistant and student at the{" "}
+            <a target="_blank" href="https://www.cs.washington.edu/">
+              University of Washington's Paul G. Allen School
+            </a>
+            . Currently pursuing my passion for web development, teaching, and
+            environmental sustainability.
           </p>
         </ParallaxLayer>
         <ParallaxLayer offset={1.5} speed={0.8} className="project-container">
-          <section>
+          <section id="syntext-border">
             <article>
               <h2>SYNTEXT</h2>
               <p>
-                Syntext is typing practice website made with programmers in mind!
-                Through a collection of code snippets you can choose from, you can
-                hone your syntax typing skills to increase efficiency and speed.
+                Syntext is typing practice website made with programmers in
+                mind! Through a collection of code snippets you can choose from,
+                you can hone your syntax typing skills to increase efficiency
+                and speed.
               </p>
               <article>
                 <p>React.js</p>
@@ -48,44 +56,58 @@ const Testing = () => {
                 <p>Heroku</p>
               </article>
               <nav>
-                <img src={process.env.PUBLIC_URL + "/github.png"} alt="github logo"/>
-                <img src={process.env.PUBLIC_URL + "/discord.png"} alt="discord logo"/>
+                <a href="https://github.com/hcp-uw/syntext">
+                  <img
+                  src={process.env.PUBLIC_URL + "/github.png"}
+                  alt="github logo"/>
+                </a>
+                <a href="https://discord.gg/QTAzXCpnvs">
+                  <img
+                  src={process.env.PUBLIC_URL + "/discord.png"}
+                  alt="discord logo"/>
+                </a>
               </nav>
             </article>
             <div id="syntext-logo"></div>
             {/* <img src={process.env.PUBLIC_URL + "/syntext.png"} alt="syntext logo"/> */}
           </section>
-          <section>
+          <section id="m2-border">
             <div id="m2-logo"></div>
             <article>
               <h2>MEET TOGETHER</h2>
               <p>
-                Inspired by When2meet, Meet Together employed a k-means algorithm
-                to allow organizers divide participants into interest based clusterings.
-                With this, you are able to use Google Forms to divide responses based on
-                commonality.
+                Inspired by When2meet, Meet Together employed a k-means
+                algorithm to allow organizers divide participants into interest
+                based clusterings. With this, you are able to use Google Forms
+                to divide responses based on commonality.
               </p>
               <article>
                 <p>React.js</p>
                 <p>•</p>
                 <p>Tailwind</p>
-                <p>•</p>
-                <p>Google Forms API</p>
               </article>
               <nav>
-                <img src={process.env.PUBLIC_URL + "/github.png"} alt="github logo"/>
-                <img src={process.env.PUBLIC_URL + "/discord.png"} alt="discord logo"/>
+                <a href="https://github.com/elimelt/meet-together">
+                  <img
+                  src={process.env.PUBLIC_URL + "/github.png"}
+                  alt="github logo"/>
+                </a>
+                <a href="https://discord.gg/9jkFXg2kXR">
+                  <img
+                  src={process.env.PUBLIC_URL + "/discord.png"}
+                  alt="discord logo"/>
+                </a>
               </nav>
             </article>
             {/* <img src={process.env.PUBLIC_URL + "/syntext.png"} alt="syntext logo"/> */}
           </section>
         </ParallaxLayer>
         <ParallaxLayer offset={3} speed={0.8} className="">
-          <img src={process.env.PUBLIC_URL + "/rasmus.jpg"}/>
+          <img src={process.env.PUBLIC_URL + "/rasmus.jpg"} />
         </ParallaxLayer>
       </Parallax>
     </div>
-  )
-}
+  );
+};
 
 export default Testing;
